5a82ac33e130ba72f61c3d3426fc0465813eecc7
it2yc <ityc061@gmail.com>
2024-07-12 20:48:33 +0700

    Add files via upload
    เขียนโดย ilv

---
+++ main.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands, tasks
+import asyncio
+
+intents = discord.Intents.default()
+intents.message_content = True  
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+status_cycle = [discord.Status.online, discord.Status.idle, discord.Status.dnd, discord.Status.invisible]
+change_status_task = None
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}!')
+
+async def change_status():
+    while True:
+        for status in status_cycle:
+            await bot.change_presence(status=status)
+            await asyncio.sleep(5)  
+
+@bot.event
+async def on_message(message):
+    global change_status_task
+
+    if message.author == bot.user:
+        return
+
+    if message.content == '+':
+        if change_status_task is None:
+            change_status_task = bot.loop.create_task(change_status())
+            await message.channel.send('Bot will now change status every 5 seconds.')
+
+bot.run("")
